20b0fe43acd875c0d6625d03fed2b9ea25d2e09f
marcond <marcond@gmail.com>
2021-01-15 18:32:01 +0000

    Adicionadas referencias aos codigos-fonte.

--- upload-image/test.php
+++ upload-image/test.php
@@ -12,15 +12,28 @@
     <input type="hidden" name="api-id" value="desenvolvimento">
     <input type="hidden" name="api-key" value="12345678-9abc-def0-123456789abcdef01">
 
-    <p><label for="file">Arquivo de imagem:
+    <!-- O arquivo eh enviado no parametro 'file' -->
+    <p>
+      <label for="file">Arquivo de imagem:</label>
       <input type="file" name="file">
-    </label></p>
-    <p><label for="wc-store">Nome da loja:
+    </p>
+
+    <!-- O nome da loja eh enviado no parametro 'wc-store' -->
+    <p>
+      <label for="wc-store">Nome da loja:</label>
       <input type="text" name="wc-store">
-	    </label></p>
+    </p>
 
     <p><input type="submit" name="enviar" value="Enviar arquivo..." /></p>
   </form>
+
+  <h2>Informações úteis:</h2>
+  <p><b>Código deste teste:</b> <a href='https://github.com/marcond/payment_gateway_switcher/blob/master/upload-image/test.php' target='_blank'>
+    https://github.com/marcond/payment_gateway_switcher/blob/master/upload-image/test.php
+  </a></p>
+  <p><b>Implementação da API:</b> <a href='https://github.com/marcond/payment_gateway_switcher/blob/master/upload-image/index.php' target='_blank'>
+    https://github.com/marcond/payment_gateway_switcher/blob/master/upload-image/index.php
+  </a></p>
 </body>
 </html>
 
